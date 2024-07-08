fa32cf67aef2876637e099704a6ea95a58f7fc40
José Victor <jose08.victor@gmail.com>
2024-07-08 20:03:09 -0300

    feat: routes added.

--- adm_painel/src/App.tsx
+++ adm_painel/src/App.tsx
@@ -1,11 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Header } from "./components/header"
 import "./styles.css"
+import { AddProduct } from "./pages/addProduct"
+import { ProductList } from "./pages/productList"
 
 function App () {
   return (
-    <>
+    <BrowserRouter>
+
       <Header />
-    </>
+
+      <Routes>
+        <Route path="/" element={ <AddProduct marca={"avon"} /> } />
+        <Route path="/avon/lista-dos-produtos" element={ <ProductList marca={"avon"} produtos={ "disponivel" } /> } />
+        <Route path="/avon/produtos-indisponiveis" element={ <ProductList marca={"avon"} produtos={ "indisponivel" } /> } />
+
+        <Route path="/natura" element={ <AddProduct marca={"natura"} /> } />
+        <Route path="/natura/lista-dos-produtos" element={ <ProductList marca={"natura"} produtos={ "disponivel" } /> } />
+        <Route path="/natura/produtos-indisponiveis" element={ <ProductList marca={"natura"} produtos={ "indisponivel" } /> } />
+      </Routes>
+    </BrowserRouter>
   )
 }
 

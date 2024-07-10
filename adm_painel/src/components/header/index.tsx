87f0021b072728e800ef50c45d5054110c1d9de5
José Victor <jose08.victor@gmail.com>
2024-07-10 19:06:53 -0300

    feat: NavLink and toggleClass function added.

--- adm_painel/src/components/header/index.tsx
+++ adm_painel/src/components/header/index.tsx
@@ -1,9 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 //import avon_icon from "../../assets/avon_icon.png";
 //import natura_icon from "../../assets/natura_icon.png";
 import "./styles.css"
 
 export const Header = () => {
+
+    const toggleClass = () => document.querySelector( "nav" )?.classList.toggle( "toggle" )
+
     return (
         <header className="header">
             <Link to="/"><img className="logo" src="https://img.freepik.com/psd-gratuitas/circulo-vermelho_23-2150588573.jpg?semt=sph" alt="Test" /></Link>
@@ -12,19 +15,19 @@ export const Header = () => {
                 <input type="checkbox" id="menu-hamburguer" />
 
                 <label htmlFor="menu-hamburguer">
-                    <div className="menu">
+                    <div className="menu" onClick={ toggleClass } >
                         <span className="hamburguer"></span>
                     </div>
                 </label>
 
                 <ul>
                     <li className="links">
-                        <Link to="/"><p>Produtos Avon</p></Link>
+                        <NavLink to="/avon" state={ "/avon" } onClick={ toggleClass }><p>Produtos Avon</p></NavLink>
 
-                        <Link to="/natura"><p>Produtos Natura</p></Link>
+                        <NavLink to="/natura" state={ "/natura" } onClick={ toggleClass } ><p>Produtos Natura</p></NavLink>
                     </li>
 
-                    <li className="btn">
+                    <li className="login-btn">
                         <button>Log out</button>
                     </li>
                 </ul>

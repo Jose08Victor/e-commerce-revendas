062b26b01714f7ef46a926a5ee3662321a674f15
José Victor <jose08.victor@gmail.com>
2024-08-01 19:04:33 -0300

    feat: styled-components and themeColor imported.

--- adm_painel/src/pages/productList/index.tsx
+++ adm_painel/src/pages/productList/index.tsx
@@ -2,23 +2,22 @@ import { useContext, useEffect } from "react";
 import { AdminContext } from "../../context/adminContext";
 import { RemovalPopUp } from "../../components/removalPopUp";
 import { EditPopUp } from "../../components/editPopUp";
-import "./styles.css";
+import { CategoryField, ListTable, ListTableFormat, ProductListContainer } from "./styles";
 
 export const ProductList = ( { produtos }: { produtos: string } ) => {
     const adminContext = useContext( AdminContext );
 
     if ( !adminContext ) throw new Error( 'useContext deve ser usado dentro de um AdminContextProvider' );
 
-    const { url, brand, list, productPopUp, setProductPopUp, fetchList } = adminContext;
+    const { url, brand, list, productPopUp, setProductPopUp, fetchList, themeColor } = adminContext;
 
     useEffect( () => {
         fetchList();
     }, [] )
 
     return (
-        <section className="products-list">
-            <ul className="category-field">
-
+        <ProductListContainer theme={themeColor}>
+            <CategoryField theme={themeColor}>
                 <li>perfumes</li>
                 <li>maquiagem</li>
                 <li>Kits</li>
@@ -30,22 +29,22 @@ export const ProductList = ( { produtos }: { produtos: string } ) => {
                 <li>maquiagem</li>
                 <li>maquiagem</li>
                 <li>maquiagem</li>
-            </ul>
+            </CategoryField>
 
-            <ul className="list-table-format">
+            <ListTableFormat theme={themeColor}>
                 <li>Imagem</li>
                 <li>Nome</li>
                 <li>Preço</li>
                 <li>Validade</li>
                 <li>Quantidade</li>
                 <li>Ação</li>
-            </ul>
+            </ListTableFormat>
 
             { list.map( ( product, index ) => {
                 if ( produtos === "disponivel" ? product.quantity > 0 : product.quantity === 0 )
                     return (
-                        <div className="list-table" key={ index }>
-                            <ul className="list-table-format">
+                        <ListTable theme={themeColor} key={ index }>
+                            <ListTableFormat theme={themeColor}>
                                 <li><img src={ `${ url }/images/${ brand }/${ product.image }` } alt="Imagem do produto" /></li>
 
                                 <li>{ product.name }</li>
@@ -63,12 +62,12 @@ export const ProductList = ( { produtos }: { produtos: string } ) => {
 
                                     { productPopUp?.id === product._id && productPopUp.action === "removal" && <RemovalPopUp productId={ product._id } /> }
                                 </li>
-                            </ul>
+                            </ListTableFormat>
 
                             { productPopUp?.id === product._id && productPopUp.action === "edit" && <EditPopUp productImage={ product.image } /> }
-                        </div>
+                        </ListTable>
                     )
             } ) }
-        </section>
+        </ProductListContainer>
     )
 }

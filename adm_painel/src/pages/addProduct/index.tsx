3c0c11f5048a2cfd613256ce6b54d7d0b4c94d00
José Victor <jose08.victor@gmail.com>
2024-08-01 19:06:15 -0300

    feat: styled-components and themeColor imported.

--- adm_painel/src/pages/addProduct/index.tsx
+++ adm_painel/src/pages/addProduct/index.tsx
@@ -1,37 +1,38 @@
-import "./styles.css"
-import upload_area from "../../assets/cloud-upload.svg"
+import pink_cloud_upload from "../../assets/pink_cloud_upload.svg";
+import orange_cloud_upload from "../../assets/orange_cloud_upload.svg";
 import { useContext } from "react";
 import { AdminContext } from "../../context/adminContext";
+import { AddButton, AddCategory_Price, AddImgUpload, AddName, AddProductFields, AddValidity_Quantity, FormContainer, InputFields } from "./styles";
 
 export const AddProduct = () => {
   const adminContext = useContext( AdminContext );
 
   if ( !adminContext ) throw new Error( 'useContext deve ser usado dentro de um AdminContextProvider' );
 
-  const { data, setData, onChangeHandler, onChangeInput, addProduct } = adminContext;
+  const { themeColor, data, setData, onChangeHandler, onChangeInput, addProduct, brand } = adminContext;
 
   return (
-    <form className="form-container" onSubmit={ addProduct } >
-      <div className="add-img-upload">
+    <FormContainer onSubmit={ addProduct } >
+      <AddImgUpload>
         <p>Escolha uma imagem :</p>
 
         <label htmlFor="image">
-          <img src={ data.image ? URL.createObjectURL( data.image ) : upload_area } alt="Upload Area Image" />
+          <img src={ data.image ? URL.createObjectURL( data.image ) : brand === "avon" ? pink_cloud_upload : orange_cloud_upload } alt="Upload Area Image" />
         </label>
 
         <input onChange={ ( e ) => setData( data => ( { ...data, [ e.target.name ]: e.target.files ? e.target.files[ 0 ] : null } ) ) } type="file" id="image" name="image" hidden required />
-      </div>
+      </AddImgUpload>
 
-      <div className="input-fields">
-        <div className="add-product-fields">
-          <div className="add-name">
+      <InputFields>
+        <AddProductFields theme={ themeColor }>
+          <AddName>
             <p>Nome :</p>
 
             <input onChange={ onChangeHandler } value={ data.name } type="text" name="name" placeholder="Digite aqui..." required />
-          </div>
+          </AddName>
 
-          <div className="add-category-price">
-            <div className="add-category">
+          <AddCategory_Price>
+            <div>
               <p>Categoria :</p>
 
               <select onChange={ onChangeHandler } value={ data.category } name="category" required>
@@ -42,30 +43,30 @@ export const AddProduct = () => {
               </select>
             </div>
 
-            <div className="add-price">
+            <div>
               <p>Preço :</p>
 
               <input onChange={ onChangeHandler } value={ data.price } type="Number" name="price" placeholder="R$ -" min={ 0 } max={ 999 } required />
             </div>
-          </div>
+          </AddCategory_Price>
 
-          <div className="add-validity-quantity">
-            <div className="add-validity">
+          <AddValidity_Quantity>
+            <div>
               <p>Validade :</p>
 
               <input type="text" value={ data.validity } name="validity" onChange={ onChangeInput } placeholder="DD/MM/YYYY" maxLength={ 10 } required />
             </div>
 
-            <div className="add-quantity">
+            <div>
               <p>Quantidade :</p>
 
               <input type="number" value={ data.quantity } name="quantity" onChange={ onChangeHandler } placeholder="-" min={ 0 } max={ 99 } required />
             </div>
-          </div>
-        </div>
+          </AddValidity_Quantity>
+        </AddProductFields>
 
-        <button type="submit" className="add-btn">Adicionar</button>
-      </div>
-    </form>
+        <AddButton theme={ themeColor } type="submit">Adicionar</AddButton>
+      </InputFields>
+    </FormContainer>
   );
 };

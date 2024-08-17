53d539f3d958a8832f970cc326081f4766853526
José Victor <jose08.victor@gmail.com>
2024-08-17 14:09:37 -0300

    feat: separate schemas for Avon products and kits.

--- server/models/avonModel.js
+++ server/models/avonModel.js
@@ -1,14 +1,21 @@
 import mongoose from "mongoose";
 
-const avonSchema = new mongoose.Schema({
+const avonProductSchema = new mongoose.Schema({
     name: { type: String, required: true },
     image: { type: String, required: true },
     price: { type: Number, required: true },
     category: { type: String, required: true },
     quantity: { type: Number, required: true },
     validity: { type: String, required: true },
-})
+});
 
-const avonModel = mongoose.models.avonProducts || mongoose.model("avonProducts", avonSchema);
+const avonKitSchema = new mongoose.Schema({
+    nameOfProducts: {type: Array, required: true},
+    image: { type: String, required: true },
+    price: { type: Number, required: true },
+    quantity: { type: Number, required: true },
+});
 
-export default avonModel;
+export const avonProductModel = mongoose.models.avonProducts || mongoose.model("avonProducts", avonProductSchema);
+
+export const avonKitModel = mongoose.models.avonKits || mongoose.model("avonKits", avonKitSchema);

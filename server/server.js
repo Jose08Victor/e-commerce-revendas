7d20acc9a460328427fefe78b14401e15f4b829f
José Victor <jose08.victor@gmail.com>
2024-08-17 14:46:35 -0300

    feat: add magazine data routes and static image handling.

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 import { connectDB } from "./config/db.js";
 import express from "express";
 import cors from "cors";
-import "dotenv/config"
+import "dotenv/config";
 import avonRouter from "./routes/avonRoute.js";
 import naturaRouter from "./routes/naturaRoute.js";
+import magazineDataRouter from "./routes/magazineDataRoute.js";
 
 //app config
- const app = express();
- const port = 4000;
+const app = express();
+const port = 4000;
 
 //middleware
 app.use(express.json());
@@ -17,15 +18,14 @@ app.use(cors());
 connectDB();
 
 //API endpoints
-app.use("/api/avon", avonRouter)
-app.use("/images/avon", express.static('uploads/avon'))
-app.use("/api/natura", naturaRouter)
-app.use("/images/natura", express.static('uploads/natura'))
+app.use("/api/avon", avonRouter);
+app.use("/api/natura", naturaRouter);
+app.use("/api/magazineData", magazineDataRouter);
 
-app.get("/", (_, res) => {
-    res.send("API Working")
-})
+app.use("/images/avon", express.static('uploads/avon'));
+app.use("/images/natura", express.static('uploads/natura'));
+app.use("/images/magazineData", express.static('uploads/magazineImage'));
 
-app.listen(port, () => {
-    console.log(`Server Started on http://localhost:${port}`);
-})
+app.get("/", (_, res) => res.send("API Working"));
+
+app.listen(port, () => console.log(`Server Started on http://localhost:${port}`));

7adfb63ee3f9484cfb4b2cbc85447fa01e42d42e
José Victor <jose08.victor@gmail.com>
2024-08-17 14:39:51 -0300

    feat: add support for Avon kits and refactor product management.

--- server/controllers/avonController.js
+++ server/controllers/avonController.js
@@ -1,71 +1,119 @@
-import avonModel from "../models/avonModel.js";
+import { avonProductModel, avonKitModel } from "../models/avonModel.js";
 import fs from "fs";
 
-const addAvonProduct = async (req, res) => {
-    const avon = new avonModel({
+export const addAvonProduct = async (req, res) => {
+    const avon = new avonProductModel({
         name: req.body.name,
         image: req.file.filename,
         price: Number(req.body.price),
         category: req.body.category,
         quantity: Number(req.body.quantity),
         validity: req.body.validity
-    })
+    });
     try {
         await avon.save();
-        res.json({ success: true, message: "Produto Adicionado" })
+        res.json({ success: true, message: "Produto Adicionado" });
     } catch (error) {
         console.log(error);
-        res.json({ success: false, message: "Erro ao adicionar o produto" })
-    }
-}
+        res.json({ success: false, message: "Erro ao adicionar o produto" });
+    };
+};
 
-
-const avonProductList = async (_, res) => {
+export const addAvonKit = async (req, res) => {
+    const avon = new avonKitModel({
+        nameOfProducts: req.body.nameOfProducts.split("//"),
+        image: req.file.filename,
+        price: Number(req.body.price),
+        quantity: Number(req.body.quantity)
+    });
     try {
-        const avonProducts = await avonModel.find({})
-        res.json({ success: true, data: avonProducts })
+        await avon.save();
+        res.json({ success: true, message: "Kit Adicionado" });
     } catch (error) {
         console.log(error);
-        res.json({ success: false, message: "Error" })
-    }
-}
+        res.json({ success: false, message: "Erro ao adicionar o kit" });
+    };
+};
 
-
-const updateAvonProduct = async (req, res) => {
+export const avonProductList = async (_, res) => {
     try {
-        const product = await avonModel.findById(req.params.id);
+        const avonProducts = await avonProductModel.find({});
+        res.json({ success: true, data: avonProducts });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Erro ao carregar os produtos" });
+    };
+};
 
+export const avonKitList = async (_, res) => {
+    try {
+        const avonKits = await avonKitModel.find({});
+        res.json({ success: true, data: avonKits });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Erro ao carregar os kits" });
+    };
+};
+
+export const updateAvonProduct = async (req, res) => {
+    try {
+        const product = await avonProductModel.findById(req.params.id);
         const data = { ...req.body };
+
         if (req.file) {
             fs.unlink(`uploads/avon/${product.image}`, () => { });
             data.image = `${req.file.filename}`;
-        }
+        };
 
-        await avonModel.findByIdAndUpdate(req.params.id, data);
-
+        await avonProductModel.findByIdAndUpdate(req.params.id, data);
+        res.json({ success: true, message: "Produto Atualizado" });
+        
     } catch (error) {
         console.log(error);
+        res.json({ success: false, message: "Erro ao atualizar o produto" });
+    };
+};
 
-        res.json({ success: false, message: "Erro ao atualizar o produto" })
-    }
-    res.json({ success: true, message: "Produto Atualizado" })
-}
-
-
-const deleteAvonProduct = async (req, res) => {
-
+export const updateAvonKit = async (req, res) => {
     try {
-        const product = await avonModel.findById(req.params.id);
+        const kit = await avonKitModel.findById(req.params.id);
+        const data = { ...req.body };
 
+        if (req.file) {
+            fs.unlink(`uploads/avon/${kit.image}`, () => { });
+            data.image = `${req.file.filename}`;
+        };
+
+        if (req.body.nameOfProducts) data.nameOfProducts = req.body.nameOfProducts.split("//");
+        await avonKitModel.findByIdAndUpdate(req.params.id, data);
+
+        res.json({ success: true, message: "Kit Atualizado" });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Erro ao atualizar o kit" });
+    };
+};
+
+export const deleteAvonProduct = async (req, res) => {
+    try {
+        const product = await avonProductModel.findById(req.params.id);
         fs.unlink(`uploads/avon/${product.image}`, () => { });
-
-        await avonModel.findByIdAndDelete(req.params.id);
-
+        await avonProductModel.findByIdAndDelete(req.params.id);
         res.json({ success: true, message: "Produto Removido" });
     } catch (error) {
         console.log(error);
-        res.json({ success: true, message: "Falha ao remover o produto" })
-    }
-}
+        res.json({ success: false, message: "Falha ao remover o produto" });
+    };
+};
 
-export { addAvonProduct, avonProductList, updateAvonProduct, deleteAvonProduct }
+export const deleteAvonKit = async (req, res) => {
+    try {
+        const kit = await avonKitModel.findById(req.params.id);
+        fs.unlink(`uploads/avon/${kit.image}`, () => { });
+        await avonKitModel.findByIdAndDelete(req.params.id);
+        res.json({ success: true, message: "Kit Removido" });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Falha ao remover o kit" });
+    };
+};

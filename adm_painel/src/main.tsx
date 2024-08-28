3e782de69b4bf51b27a20e92b7d26d58de715e27
José Victor <jose08.victor@gmail.com>
2024-08-28 12:28:10 -0300

    feat: add global styles and AdminContextProvider to application root.

--- adm_painel/src/main.tsx
+++ adm_painel/src/main.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import AdminContextProvider from './context/adminContext.tsx'
+import { GlobalStyle } from './globalStyle.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot( document.getElementById( 'root' )! ).render(
   <React.StrictMode>
-    <App />
+    <AdminContextProvider>
+      <GlobalStyle />
+      <App />
+    </AdminContextProvider>
   </React.StrictMode>,
-)
+);

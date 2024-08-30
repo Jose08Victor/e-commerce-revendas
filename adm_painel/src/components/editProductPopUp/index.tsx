2b90a5da40054ffc0ae74d53f3b431e65f0214a8
José Victor <jose08.victor@gmail.com>
2024-08-30 18:31:44 -0300

    feat: "editProductPopUp" component created.

---
+++ adm_painel/src/components/editProductPopUp/index.tsx
@@ -0,0 +1,112 @@
+import { useContext } from "react";
+import { AdminContext } from "../../context/adminContext";
+import pink_cloud_upload from "../../assets/pink_cloud_upload.svg";
+import orange_cloud_upload from "../../assets/orange_cloud_upload.svg";
+import arrow_icon from "../../assets/arrow_icon.png";
+import { EditCategory_Price, EditName, EditSelect, EditInput, EditValidity_Quantity } from "./styles";
+import { ArrowIcon, CurrentImg, EditTitle, EditBackground, EditContainer, ImgContainer, ImgUpload, EditButton } from "../styles/componentStyles";
+
+export const EditProductPopUp = ( { productImage }: { productImage: File } ) => {
+    const adminContext = useContext( AdminContext );
+
+    if ( !adminContext ) throw new Error( 'useContext deve ser usado dentro de um AdminContextProvider' );
+
+    const { themeColor, url, brand, productData, setProductData, setPopUp, updateProduct, onChangeHandler, onChangeValidityInput } = adminContext;
+
+    return (
+        <EditBackground>
+            <EditContainer theme={ themeColor } onSubmit={ updateProduct } >
+                <EditTitle theme={ themeColor }>
+                    <h1>Editar produto :</h1>
+
+                    <p onClick={ () => {
+                        setPopUp( null );
+                        setProductData( {
+                            name: "",
+                            image: null,
+                            price: "",
+                            category: "",
+                            quantity: "",
+                            validity: ""
+                        } );
+                    } }>x</p>
+                </EditTitle>
+
+                <ImgContainer>
+                    <div>
+                        <p>Imagem atual</p>
+
+                        <CurrentImg src={ `${ url }/images/${ brand }/${ productImage }` } alt="Imagem do produto" />
+                    </div>
+
+                    <ArrowIcon src={ arrow_icon } alt="Arrow Icon" />
+
+                    <div>
+                        <p>Trocar imagem ?</p>
+
+                        <label htmlFor="image">
+                            <ImgUpload src={ productData.image ? URL.createObjectURL( productData.image ) : brand === "avon" ? pink_cloud_upload : orange_cloud_upload } alt="Upload Area Image" />
+                        </label>
+
+                        <input onChange={ ( e ) => setProductData( productData => ( { ...productData, [ e.target.name ]: e.target.files ? e.target.files[ 0 ] : null } ) ) } type="file" id="image" name="image" hidden />
+                    </div>
+                </ImgContainer>
+
+                <EditName>
+                    <p>Editar Nome ?</p>
+
+                    <EditInput theme={ themeColor } value={ productData.name } onChange={ onChangeHandler } type="text" name="name" placeholder="Digite aqui..." />
+                </EditName>
+
+                <EditCategory_Price>
+                    <div>
+                        <p>Editar Categoria ?</p>
+
+                        <EditSelect theme={ themeColor } name="category" onChange={ onChangeHandler } >
+                            <option value="">Escolha a categoria</option>
+                            { brand === "avon" ?
+                                <>
+                                    <option value="Perfumes">Perfumes</option>
+                                    <option value="Rosto">Rosto</option>
+                                    <option value="Lábios">Lábios</option>
+                                    <option value="Olhos">Olhos</option>
+                                    <option value="Unhas">Unhas</option>
+                                    <option value="Desodorantes">Desodorantes</option>
+                                    <option value="Cuidados Diários">Cuidados Diários</option>
+                                    <option value="Casa&Estilo">Casa&Estilo</option>
+                                </>
+                                :
+                                <>
+                                    <option value="Perfumes">Perfumes</option>
+                                    <option value="Maquiagem">Maquiagem</option>
+                                    <option value="Cuidados Diários">Cuidados Diários</option>
+                                </> }
+                        </EditSelect>
+                    </div>
+
+                    <div>
+                        <p>Editar Preço ?</p>
+
+                        <EditInput theme={ themeColor } value={ productData.price } onChange={ onChangeHandler } type="Number" name="price" placeholder="R$ -" min={ 0 } max={ 999 } />
+                    </div>
+                </EditCategory_Price>
+
+                <EditValidity_Quantity>
+                    <div>
+                        <p>Editar Validade ?</p>
+
+                        <EditInput theme={ themeColor } type="text" value={ productData.validity } onChange={ onChangeValidityInput } name="validity" placeholder="DD/MM/YYYY" maxLength={ 10 } />
+                    </div>
+
+                    <div>
+                        <p>Editar Quantidade ?</p>
+
+                        <EditInput theme={ themeColor } type="number" value={ productData.quantity } onChange={ onChangeHandler } name="quantity" placeholder="-" min={ 0 } max={ 99 } />
+                    </div>
+                </EditValidity_Quantity>
+
+                <EditButton theme={ themeColor } type="submit">Confirmar</EditButton>
+            </EditContainer>
+        </EditBackground>
+    )
+}
---
+++ adm_painel/src/components/editProductPopUp/styles.tsx
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+import { Input, Select } from "../styles/componentStyles";
+
+export const EditName = styled.div`
+    width: 80%;
+    margin: 0 auto;
+
+    input {
+        width: 100%;
+    }
+
+    @media (max-width: 500px) {
+        width: 100%;
+    }
+`
+
+export const EditCategory_Price = styled(EditName)`
+    display: flex;
+    justify-content: space-between;
+
+    select {
+        text-align: center;
+        width: 200px;
+    }
+
+    input {
+        text-align: center;
+        width: 120px;
+    }
+
+    @media (max-width: 500px) {
+        select {
+            width: 140px;
+        }
+
+        input {
+            width: 100px;
+        }
+    }
+`
+
+export const EditValidity_Quantity = styled(EditCategory_Price)`
+    :first-child input {
+        width: 200px;
+    }
+
+    @media (max-width: 500px) {
+        :first-child input {
+            width: 140px;
+        }
+    }
+`
+
+export const EditInput = styled( Input )`
+    height: 30px;
+    margin-bottom: 25px;
+    
+    @media (max-width: 740px) {
+        height: 26px;
+        font-size: 13px;
+    }
+
+    @media (max-width: 375px) {
+        height: 22px;
+        font-size: 10px;
+    }
+`
+
+export const EditSelect = styled( Select )`
+    height: 30px;
+    margin-bottom: 25px;
+    
+    @media (max-width: 740px) {
+        height: 26px;
+        font-size: 13px;
+    }
+
+    @media (max-width: 375px) {
+        height: 22px;
+        font-size: 10px;
+    }
+`

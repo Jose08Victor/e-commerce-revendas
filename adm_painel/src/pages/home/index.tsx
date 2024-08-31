8004b8cd65ffb9cc8fc4947225d3cedb637ae9b9
José Victor <jose08.victor@gmail.com>
2024-08-31 12:33:38 -0300

    feat: "home" page created.

---
+++ adm_painel/src/pages/home/index.tsx
@@ -0,0 +1,129 @@
+import { useContext, useEffect, useState } from "react";
+import { AdminContext } from "../../context/adminContext";
+import { Section, DivContainer, Div, Form, Input, ButtonContainer, Button, Label, Label1, H1, H2, ImgContainerDiv, MagazineContainer, ImgContainer, Test } from "./styles";
+import { Link } from "react-router-dom";
+import { EditMagazinePopUp } from "../../components/editMagazinePopUp";
+
+export const Home = () => {
+    const adminContext = useContext( AdminContext );
+
+    if ( !adminContext ) throw new Error( 'useContext deve ser usado dentro de um AdminContextProvider' );
+
+    const { onChangeValidityInput, onChangeHandler, magazineData, setMagazineData, getMagazineList, updateMagazineData, list, themeColor, url, popUp, setPopUp } = adminContext;
+
+    useEffect( () => { getMagazineList() }, [] );
+
+    useEffect( () => setCode( "" ), [ list ] );
+
+    const [ code, setCode ] = useState( "" );
+
+    const setHandler = ( code: string ) => {
+        setCode( code );
+        setMagazineData( {
+            currentCycle: "",
+            startOfCycle: "",
+            endOfCycle: "",
+            avonMagazineImage: null,
+            avonMagazineLink: "",
+            casa_estiloMagazineImage: null,
+            casa_estiloMagazineLink: "",
+            naturaMagazineImage: null,
+            naturaMagazineLink: ""
+        } );
+    }
+
+    if ( list.magazine[ 0 ] ) {
+        return (
+            <Section>
+                <DivContainer>
+                    { code !== "Ciclo" && <Div>
+                        <H1 theme={ themeColor }>Ciclo Atual : <span>{ `${ list.magazine[ 0 ].currentCycle }` }</span></H1>
+
+                        <Button theme={ themeColor } onClick={ () => setHandler( "Ciclo" ) }>Trocar Ciclo ?</Button>
+                    </Div> }
+
+                    { code === "Ciclo" && <Form theme={ themeColor } onSubmit={ updateMagazineData }>
+                        <H2>Trocar Ciclo :</H2>
+
+                        <Label htmlFor="ciclo">Ciclo :
+                            <Input theme={ themeColor } onChange={ onChangeHandler } value={ magazineData.currentCycle.replace( /\D/g, '' ) } type="text" id="ciclo" maxLength={ 2 } name="currentCycle" required />
+                        </Label>
+
+                        <ButtonContainer>
+                            <Button theme={ themeColor } type="submit">Confirmar</Button>
+                            <Button theme={ themeColor } type="button" onClick={ () => setCode( "" ) }>Cancelar</Button>
+                        </ButtonContainer>
+
+                    </Form> }
+
+                    { code !== "Périodo" && <Div>
+                        <H1 theme={ themeColor }>Período : <span>{ `${ list.magazine[ 0 ].startOfCycle }` }</span> até <span>{ `${ list.magazine[ 0 ].endOfCycle }` }</span></H1>
+
+                        <Button theme={ themeColor } onClick={ () => setHandler( "Périodo" ) }>Trocar Périodo ?</Button>
+                    </Div> }
+
+                    { code === "Périodo" && <Form theme={ themeColor } onSubmit={ updateMagazineData }>
+                        <H2>Trocar Período :</H2>
+                        <Label1 htmlFor="start">Início :
+                            <Input theme={ themeColor } id="start" type="text" value={ magazineData.startOfCycle } name="startOfCycle" onChange={ onChangeValidityInput } placeholder="DD/MM" maxLength={ 5 } required />
+                        </Label1>
+
+                        <Label htmlFor="end">Término :
+                            <Input theme={ themeColor } id="end" type="text" value={ magazineData.endOfCycle } name="endOfCycle" onChange={ onChangeValidityInput } placeholder="DD/MM" maxLength={ 5 } required />
+                        </Label>
+
+                        <ButtonContainer>
+                            <Button theme={ themeColor }>Confirmar</Button>
+                            <Button theme={ themeColor } onClick={ () => setCode( "" ) }>Cancelar</Button>
+                        </ButtonContainer>
+                    </Form> }
+                </DivContainer>
+
+                <ImgContainerDiv>
+                    <MagazineContainer>
+                        <h2>Revista Avon :</h2>
+
+                        <ImgContainer theme={ themeColor }>
+                            <img src={ `${ url }/images/magazineData/${ list.magazine[ 0 ].avonMagazineImage }` } alt="" />
+
+                            <Test>
+                                <Link to={ list.magazine[ 0 ].avonMagazineLink } target="_blank" ><Button theme={ themeColor }>Ver Revista</Button></Link>
+
+                                <Button theme={ themeColor } onClick={ () => setPopUp( { action: "edit magazine", image: list.magazine[ 0 ]?.avonMagazineImage, imageName: "avonMagazineImage", linkName: "avonMagazineLink", brand: "avon" } ) }>Trocar Revista ?</Button>
+                            </Test>
+                        </ImgContainer>
+                    </MagazineContainer>
+
+                    <MagazineContainer>
+                        <h2>Revista Casa & Estilo :</h2>
+
+                        <ImgContainer theme={ themeColor }>
+                            <img src={ `${ url }/images/magazineData/${ list.magazine[ 0 ].casa_estiloMagazineImage }` } alt="" />
+
+                            <Test>
+                                <Link to={ list.magazine[ 0 ].casa_estiloMagazineLink } target="_blank" ><Button theme={ themeColor }>Ver Revista</Button></Link>
+
+                                <Button theme={ themeColor } onClick={ () => setPopUp( { action: "edit magazine", image: list.magazine[ 0 ]?.casa_estiloMagazineImage, imageName: "casa_estiloMagazineImage", linkName: "casa_estiloMagazineLink", brand: "casa&estilo" } ) }>Trocar Revista ?</Button>
+                            </Test>
+                        </ImgContainer>
+                    </MagazineContainer>
+
+                    <MagazineContainer>
+                        <h2>Revista Natura :</h2>
+
+                        <ImgContainer theme={ themeColor }>
+                            <img src={ `${ url }/images/magazineData/${ list.magazine[ 0 ].naturaMagazineImage }` } alt="" />
+
+                            <Test>
+                                <Link to={ list.magazine[ 0 ].naturaMagazineLink } target="_blank"><Button theme={ themeColor }>Ver Revista</Button></Link>
+
+                                <Button theme={ themeColor } onClick={ () => setPopUp( { action: "edit magazine", image: list.magazine[ 0 ]?.naturaMagazineImage, imageName: "naturaMagazineImage", linkName: "naturaMagazineLink", brand: "natura" } ) }>Trocar Revista ?</Button>
+                            </Test>
+                        </ImgContainer>
+                    </MagazineContainer>
+                </ImgContainerDiv>
+                { popUp?.action === "edit magazine" && <EditMagazinePopUp /> }
+            </Section>
+        )
+    }
+}
---
+++ adm_painel/src/pages/home/styles.tsx
@@ -0,0 +1,167 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    height: 100%;
+    width: 100%;
+    padding-top: 20px;
+    gap: 20px;
+    flex-direction: column;
+    text-align: center;
+`
+
+export const DivContainer = styled.div`
+    display: flex; 
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-evenly;
+    width: 100%;
+`
+
+export const Div = styled.div`
+    width: 500px;
+    height: 160px;
+    max-width: 300px;
+    border: 2px solid transparent;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+export const Form = styled.form`
+    width: 500px;
+    height: 160px;
+    max-width: 300px;
+    border: 2px solid ${ props => props.theme };
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`
+
+export const H1 = styled.h1`
+    margin-bottom: 20px;
+
+    span {
+        border-bottom: 2px solid ${ props => props.theme };
+    }
+`
+
+export const H2 = styled.h2`
+    margin-bottom: 10px;
+`
+
+export const Label = styled.label`
+   font-size: 17px;
+   font-weight: 500;
+`
+
+export const Label1 = styled( Label )`
+    margin-left: 20px;
+`
+
+export const Input = styled.input`
+    width: 114px;
+    text-align: center;
+    height: 28px;
+    font-size: 15px;
+    font-family: "Nunito", sans-serif;
+    border: 1px solid ${ props => props.theme };
+    border-radius: 10px;
+    outline: none;
+    margin-bottom: 10px;
+    margin-left: 5px;
+
+    &:focus {
+        border: 2px solid ${ props => props.theme };
+    }
+    
+    @media (max-width: 740px) {
+        height: 28px;
+        font-size: 13.5px;
+        padding-left: 6px;
+    }
+
+    @media (max-width: 375px) {
+        height: 23px;
+        font-size: 10px;
+        padding-left: 4px;
+    }
+`
+
+export const ButtonContainer = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+`
+
+export const Button = styled.button`
+    font-size: 13px;
+    border-radius: 12px;
+    border: 2px solid;
+    padding: 7px 12px;
+    color: aliceblue;
+    background-color: ${ props => props.theme };
+
+    &:hover {
+        color: ${ props => props.theme };
+        background-color: aliceblue;
+        border: 2px solid ${ props => props.theme };
+        cursor: pointer;
+    }
+`
+
+export const ImgContainerDiv = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-evenly;
+    width: 100%;
+
+    @media(max-width: 900px) {
+        justify-content: center;
+        gap: 80px;
+        padding: 0 20px;
+    }
+`
+
+export const MagazineContainer = styled.div`
+    h2 {
+        margin-bottom: 10px;
+    }
+`
+
+export const ImgContainer = styled.div`
+    position: relative;
+
+    img {
+        width: 250px;
+        display: flex;
+        border: 3px solid ${ props => props.theme };
+        
+        &:hover~div {
+            display: flex;
+        }
+    }
+`
+
+export const Test = styled.div`
+    position: absolute;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    gap: 20px 0;
+    display: none;
+
+    &:hover {
+        display: flex;
+    }
+`

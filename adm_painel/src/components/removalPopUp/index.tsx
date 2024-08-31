cc59b684066ff8df6dc8ebd3b3344d727b973d7f
José Victor <jose08.victor@gmail.com>
2024-08-31 12:37:52 -0300

    refactor: update RemovalPopUp component to handle multiple item types.

--- adm_painel/src/components/removalPopUp/index.tsx
+++ adm_painel/src/components/removalPopUp/index.tsx
@@ -2,23 +2,23 @@ import { useContext } from "react";
 import { AdminContext } from "../../context/adminContext";
 import { RemovalPopUpContainer } from "./styles";
 
-export const RemovalPopUp = ({ productId }: { productId: string }) => {
+export const RemovalPopUp = ( { productId }: { productId: string } ) => {
     const adminContext = useContext( AdminContext );
 
     if ( !adminContext ) throw new Error( 'useContext deve ser usado dentro de um AdminContextProvider' );
 
-    const { setProductPopUp, removeProduct } = adminContext;
+    const { type, setPopUp, removeProduct, removeKit } = adminContext;
 
     return (
         <RemovalPopUpContainer>
             <h4>Desejar remover este produto?</h4>
 
             <button onClick={ () => {
-                removeProduct( productId )
-                setProductPopUp( null )
+                type === "Produto" ? removeProduct( productId ) : removeKit( productId );
+                setPopUp( null );
             } } >Sim</button>
 
-            <button onClick={ () => setProductPopUp( null ) }>Não</button>
+            <button onClick={ () => setPopUp( null ) }>Não</button>
         </RemovalPopUpContainer>
     )
-}
+};

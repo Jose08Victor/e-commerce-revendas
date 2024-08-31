c9309b312003d6faf066215146277225458260ca
José Victor <jose08.victor@gmail.com>
2024-08-31 12:57:05 -0300

    refactor: enhance AdminContext with support for multiple data types.

--- adm_painel/src/context/adminContext.tsx
+++ adm_painel/src/context/adminContext.tsx
@@ -1,28 +1,36 @@
 import { createContext, useState } from "react";
-import { AdminContextProps, AdminContextProviderProps, ProductData, ProductsData, ProductPopUp } from "../types";
+import { AdminContextProps, AdminContextProviderProps, ProductData, PopUp, KitData, List, MagazineData } from "../types";
 import axios from "axios";
 import { toast } from "react-toastify";
 
 export const AdminContext = createContext<AdminContextProps | null>( null );
 
-export const themes = {
+const themes = {
     avonColor: "#E4004B",
     naturaColor: "#FF6916",
     defaultColor: "#4eb2ff"
-}
+};
 
 const AdminContextProvider = ( props: AdminContextProviderProps ) => {
     const url = "http://localhost:4000";
 
-    const [themeColor, setThemeColor] = useState<{}>(themes.defaultColor)
+    const [ themeColor, setThemeColor ] = useState<string>( themes.defaultColor );
 
     const [ brand, setBrand ] = useState<string>( "/" );
 
-    const [ list, setList ] = useState<ProductsData[] | never[]>( [] );
+    const [ type, setType ] = useState<string>( "Produto" );
 
-    const [ productPopUp, setProductPopUp ] = useState<ProductPopUp | null>( null );
+    const [ productName, setProductName ] = useState<string[] | []>( [] );
 
-    const [ data, setData ] = useState<ProductData>( {
+    const [ popUp, setPopUp ] = useState<PopUp | null>( null );
+
+    const [ list, setList ] = useState<List>( {
+        products: [],
+        kits: [],
+        magazine: []
+    } );
+
+    const [ productData, setProductData ] = useState<ProductData>( {
         name: "",
         image: null,
         price: "",
@@ -31,98 +39,264 @@ const AdminContextProvider = ( props: AdminContextProviderProps ) => {
         validity: ""
     } );
 
-    const fetchList = async () => {
-        const response = await axios.get( `${ url }/api/${ brand }/list` );
-        if ( !response.data.success ) toast.error( "Error" );
+    const [ kitData, setKitData ] = useState<KitData>( {
+        image: null,
+        nameOfProducts: [ "" ],
+        price: "",
+        quantity: ""
+    } );
 
-        setList( response.data.data );
+    const [ magazineData, setMagazineData ] = useState<MagazineData>( {
+        currentCycle: "",
+        startOfCycle: "",
+        endOfCycle: "",
+        avonMagazineImage: null,
+        avonMagazineLink: "",
+        casa_estiloMagazineImage: null,
+        casa_estiloMagazineLink: "",
+        naturaMagazineImage: null,
+        naturaMagazineLink: ""
+    } );
+
+    const getProductList = async () => {
+        try {
+            const response = await axios.get( `${ url }/api/${ brand }/productList` );
+            if ( !response.data.success ) toast.error( response.data.message );
+            else setList( list => ( { ...list, products: response.data.data } ) );
+        } catch ( error ) {
+            console.log( error );
+            toast.error( "Falha ao conectar com o servidor" );
+        }
+    };
+
+    const getKitList = async () => {
+        try {
+            const response = await axios.get( `${ url }/api/${ brand }/kitList` );
+            if ( !response.data.success ) toast.error( response.data.message );
+            else setList( list => ( { ...list, kits: response.data.data } ) );
+        } catch ( error ) {
+            console.log( error );
+            toast.error( "Falha ao conectar com o servidor" );
+        }
+    };
+
+    const getMagazineList = async () => {
+        try {
+            const response = await axios.get( `${ url }/api/magazineData/get` );
+            if ( !response.data.success ) toast.error( response.data.message );
+            else setList( list => ( { ...list, magazine: response.data.data } ) );
+        } catch ( error ) {
+            console.log( error );
+            toast.error( "Falha ao conectar com o servidor" );
+        }
     }
 
     const addProduct = async ( event: { preventDefault: () => void; } ) => {
-        event.preventDefault();
-        const formData = new FormData();
-        formData.append( "name", data.name )
-        formData.append( "price", data.price )
-        formData.append( "category", data.category )
-        formData.append( "quantity", data.quantity )
-        formData.append( "validity", data.validity )
-        if ( data.image ) formData.append( "image", data.image );
+        try {
+            event.preventDefault();
+            const formData = new FormData();
+            formData.append( "name", productData.name );
+            formData.append( "price", productData.price );
+            formData.append( "category", productData.category );
+            formData.append( "quantity", productData.quantity );
+            formData.append( "validity", productData.validity );
+            if ( productData.image ) formData.append( "image", productData.image );
 
-        const response = await axios.post( `${ url }/api/${ brand }/add`, formData )
+            const response = await axios.post( `${ url }/api/${ brand }/addProduct`, formData );
+            if ( !response.data.success ) toast.error( response.data.message );
+            else toast.success( response.data.message );
 
-        if ( !response.data.success ) toast.error( response.data.message )
+            setProductData( {
+                name: "",
+                image: null,
+                price: "",
+                category: "",
+                quantity: "",
+                validity: ""
+            } );
+        } catch ( error ) {
+            console.log( error );
+            toast.error( "Falha ao conectar com o servidor" );
+        }
+    };
 
-        toast.success( response.data.message );
-        setData( {
-            name: "",
-            image: null,
-            price: "",
-            category: "",
-            quantity: "",
-            validity: ""
-        } );
-    }
+    const addKit = async ( event: { preventDefault: () => void; } ) => {
+        try {
+            event.preventDefault();
+            const formData = new FormData();
+            formData.append( "nameOfProducts", kitData.nameOfProducts.join( "//" ) );
+            formData.append( "price", kitData.price );
+            formData.append( "quantity", kitData.quantity );
+            if ( kitData.image ) formData.append( "image", kitData.image );
+
+            const response = await axios.post( `${ url }/api/${ brand }/addKit`, formData );
+            if ( !response.data.success ) toast.error( response.data.message );
+            else toast.success( response.data.message );
+
+            setKitData( {
+                image: null,
+                nameOfProducts: [ "" ],
+                price: "",
+                quantity: ""
+            } );
+            setProductName( [] );
+        } catch ( error ) {
+            console.log( error );
+            toast.error( "Falha ao conectar com o servidor" );
+        }
+    };
 
     const updateProduct = async ( event: { preventDefault: () => void; } ) => {
-        event.preventDefault();
+        try {
+            event.preventDefault();
+            const formData = new FormData();
+            if ( productData.name ) formData.append( "name", productData.name );
+            if ( productData.price ) formData.append( "price", productData.price );
+            if ( productData.category ) formData.append( "category", productData.category );
+            if ( productData.quantity ) formData.append( "quantity", productData.quantity );
+            if ( productData.validity ) formData.append( "validity", productData.validity );
+            if ( productData.image ) formData.append( "image", productData.image );
 
-        const formData = new FormData();
-        if ( data.name ) formData.append( "name", data.name )
-        if ( data.price ) formData.append( "price", data.price )
-        if ( data.category ) formData.append( "category", data.category )
-        if ( data.quantity ) formData.append( "quantity", data.quantity )
-        if ( data.validity ) formData.append( "validity", data.validity )
-        if ( data.image ) formData.append( "image", data.image );
+            const response = await axios.put( `${ url }/api/${ brand }/updateProduct/${ popUp?.id }`, formData );
+            if ( !response.data.success ) toast.error( response.data.message );
+            else toast.success( response.data.message );
 
-        const response = await axios.put( `${ url }/api/${ brand }/update/${ productPopUp?.id }`, formData )
+            getProductList();
+            setPopUp( null );
+            setProductData( {
+                name: "",
+                image: null,
+                price: "",
+                category: "",
+                quantity: "",
+                validity: ""
+            } );
+        } catch ( error ) {
+            console.log( error );
+            toast.error( "Falha ao conectar com o servidor" );
+        }
+    };
 
-        if ( !response.data.success ) toast.error( response.data.message );
+    const updateKit = async ( event: { preventDefault: () => void; } ) => {
+        try {
+            event.preventDefault();
+            const formData = new FormData();
+            if ( kitData.nameOfProducts ) formData.append( "nameOfProducts", kitData.nameOfProducts.join( "//" ) );
+            if ( kitData.price ) formData.append( "price", kitData.price );
+            if ( kitData.quantity ) formData.append( "quantity", kitData.quantity );
+            if ( kitData.image ) formData.append( "image", kitData.image );
 
-        toast.success( response.data.message );
-        fetchList();
-        setProductPopUp( null );
-        setData( {
-            name: "",
-            image: null,
-            price: "",
-            category: "",
-            quantity: "",
-            validity: ""
-        } );
-    }
+            const response = await axios.put( `${ url }/api/${ brand }/updateKit/${ popUp?.id }`, formData );
+            if ( !response.data.success ) toast.error( response.data.message );
+            else toast.success( response.data.message );
+
+            getKitList();
+            setPopUp( null );
+            setProductName( [] );
+            setKitData( {
+                image: null,
+                nameOfProducts: [ "" ],
+                price: "",
+                quantity: ""
+            } );
+        } catch ( error ) {
+            console.log( error );
+            toast.error( "Falha ao conectar com o servidor" );
+        }
+    };
+
+    const updateMagazineData = async ( event: { preventDefault: () => void; } ) => {
+        try {
+            event.preventDefault();
+            const formData = new FormData();
+            if ( magazineData.currentCycle ) formData.append( "currentCycle", magazineData.currentCycle );
+            if ( magazineData.startOfCycle ) formData.append( "startOfCycle", magazineData.startOfCycle );
+            if ( magazineData.endOfCycle ) formData.append( "endOfCycle", magazineData.endOfCycle );
+            if ( magazineData.avonMagazineImage ) formData.append( "image", magazineData.avonMagazineImage );
+            if ( magazineData.avonMagazineLink ) formData.append( "avonMagazineLink", magazineData.avonMagazineLink );
+            if ( magazineData.casa_estiloMagazineImage ) formData.append( "image", magazineData.casa_estiloMagazineImage );
+            if ( magazineData.casa_estiloMagazineLink ) formData.append( "casa_estiloMagazineLink", magazineData.casa_estiloMagazineLink );
+            if ( magazineData.naturaMagazineImage ) formData.append( "image", magazineData.naturaMagazineImage );
+            if ( magazineData.naturaMagazineLink ) formData.append( "naturaMagazineLink", magazineData.naturaMagazineLink );
+            if ( popUp?.brand ) formData.append( "magazine", popUp?.brand );
+
+
+            const response = await axios.put( `${ url }/api/magazineData/update/66aec43fe9cad453353d4ec4`, formData );
+            if ( !response.data.success ) toast.error( response.data.message );
+            else toast.success( response.data.message );
+
+            getMagazineList();
+            setPopUp( null );
+            setMagazineData( {
+                currentCycle: "",
+                startOfCycle: "",
+                endOfCycle: "",
+                avonMagazineImage: null,
+                avonMagazineLink: "",
+                casa_estiloMagazineImage: null,
+                casa_estiloMagazineLink: "",
+                naturaMagazineImage: null,
+                naturaMagazineLink: ""
+            } );
+        } catch ( error ) {
+            console.log( error );
+            toast.error( "Falha ao conectar com o servidor" );
+        }
+    };
 
     const removeProduct = async ( productId: string ) => {
-        const response = await axios.delete( `${ url }/api/${ brand }/delete/${ productId }` );
-        await fetchList();
+        try {
+            const response = await axios.delete( `${ url }/api/${ brand }/deleteProduct/${ productId }` );
+            await getProductList();
+            if ( !response.data.success ) toast.error( response.data.message );
+            else toast.success( response.data.message );
 
-        if ( !response.data.success ) toast.error( response.data.message );
+        } catch ( error ) {
+            console.log( error );
+            toast.error( "Falha ao conectar com o servidor" );
+        }
+    };
 
-        toast.success( response.data.message );
-    }
+    const removeKit = async ( productId: string ) => {
+        try {
+            const response = await axios.delete( `${ url }/api/${ brand }/deleteKit/${ productId }` );
+            await getKitList();
+            if ( !response.data.success ) toast.error( response.data.message );
+            else toast.success( response.data.message );
+
+        } catch ( error ) {
+            console.log( error );
+            toast.error( "Falha ao conectar com o servidor" );
+        }
+    };
 
     const onChangeHandler = ( event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement> ) => {
         const name = event.target.name;
         const value = event.target.value;
-        setData( data => ( { ...data, [ name ]: value } ) )
-    }
 
-    const onChangeInput = ( event: React.ChangeEvent<HTMLInputElement> ) => {
+        brand === "/" && setMagazineData( magazineData => ( { ...magazineData, [ name ]: value } ) );
+        type === "Produto" && setProductData( productData => ( { ...productData, [ name ]: value } ) );
+        type === "Kit" && setKitData( kitData => ( { ...kitData, [ name ]: value } ) );
+    };
+
+    const onChangeValidityInput = ( event: React.ChangeEvent<HTMLInputElement> ) => {
         const inputValue = event.target.value.replace( /\D/g, '' ); // Remove qualquer caractere que não seja dígito
         let formattedValue = inputValue;
 
         if ( inputValue.length > 2 ) formattedValue = `${ inputValue.slice( 0, 2 ) }/${ inputValue.slice( 2 ) }`;
         if ( inputValue.length > 4 ) formattedValue = `${ formattedValue.slice( 0, 5 ) }/${ formattedValue.slice( 5 ) }`;
 
-        setData( data => ( { ...data, [ event.target.name ]: formattedValue } ) )
+        brand === "/" && setMagazineData( magazineData => ( { ...magazineData, [ event.target.name ]: formattedValue } ) );
+        type === "Produto" && setProductData( productData => ( { ...productData, [ event.target.name ]: formattedValue } ) );
     };
 
-    const contextValue = { url, themeColor, setThemeColor,brand, setBrand, data, setData, list, productPopUp, setProductPopUp, fetchList, addProduct, updateProduct, removeProduct, onChangeHandler, onChangeInput }
+    const contextValue = { url, themeColor, setThemeColor, brand, setBrand, type, setType, productData, setProductData, magazineData, setMagazineData, list, popUp, setPopUp, getProductList, getKitList, getMagazineList, addProduct, updateProduct, updateKit, updateMagazineData, removeProduct, removeKit, kitData, setKitData, productName, setProductName, addKit, onChangeHandler, onChangeValidityInput };
 
     return (
         <AdminContext.Provider value={ contextValue }>
             { props.children }
         </AdminContext.Provider>
     )
-}
+};
 
-export default AdminContextProvider
+export default AdminContextProvider;

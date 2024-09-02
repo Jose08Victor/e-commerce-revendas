89d65b8c57aadd94ce2f9da615a67f00d117f9c8
José Victor <jose08.victor@gmail.com>
2024-09-02 18:08:31 -0300

    Refactor: optimized imports.

--- adm_painel/src/main.tsx
+++ adm_painel/src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import AdminContextProvider from './context/adminContext.tsx'
-import { GlobalStyle } from './globalStyle.tsx'
+import { AdminContextProvider } from './context/adminContext.tsx';
+import { GlobalStyle } from './globalStyle.tsx';
 
 ReactDOM.createRoot( document.getElementById( 'root' )! ).render(
   <React.StrictMode>

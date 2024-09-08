959749f4579bd74ae8d65612f756dbd41445547d
José Victor <107445525+Jose08Victor@users.noreply.github.com>
2024-09-08 14:37:34 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ import magazineDataRouter from "./routes/magazineDataRoute.js";
 
 //app config
 const app = express();
-const port = 4000 || "https://marlene-cosmeticos-server.onrender.com";
+const port = process.env.PORT || 4000;
 
 //middleware
 app.use(express.json());

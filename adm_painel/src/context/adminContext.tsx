652e540c9f73de3d7d207363b731d8e4844d9218
José Victor <jose08.victor@gmail.com>
2024-09-19 17:43:36 -0300

    Refactor: url changed.

--- adm_painel/src/context/adminContext.tsx
+++ adm_painel/src/context/adminContext.tsx
@@ -12,7 +12,7 @@ export const AdminContextProvider = ( props: AdminContextProviderProps ) => {
         defaultColor: "#D8B76E"
     };
 
-    const url = "http://localhost:4000";
+    const url = "https://marlene-cosmeticos-server.onrender.com";
 
     const [ themeColor, setThemeColor ] = useState( [themes.defaultColor ] );
 

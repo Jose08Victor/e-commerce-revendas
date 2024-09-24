f1c4ffd7ec590269ae20720060bbf350d11f9ef1
José Victor <jose08.victor@gmail.com>
2024-09-24 17:36:48 -0300

    Refactor: BrowserRouter imported.

--- adm_painel/src/main.tsx
+++ adm_painel/src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { AdminContextProvider } from './context/adminContext.tsx';
 import { GlobalStyle } from './globalStyle.tsx';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot( document.getElementById( 'root' )! ).render(
   <React.StrictMode>
-    <AdminContextProvider>
-      <GlobalStyle />
-      <App />
-    </AdminContextProvider>
+    <BrowserRouter>
+      <AdminContextProvider>
+        <GlobalStyle />
+        <App />
+      </AdminContextProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 );

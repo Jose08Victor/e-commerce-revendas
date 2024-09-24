3076eecfe461e28f8a4b0e9fe00c2aae257e6ab0
José Victor <jose08.victor@gmail.com>
2024-09-24 17:35:20 -0300

    Feat: add login redirection and authentication handling.

--- adm_painel/src/App.tsx
+++ adm_painel/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import { Header } from "./components/header";
 import { AddItem } from "./pages/addItem";
 import { ProductList } from "./pages/productList";
@@ -7,26 +7,40 @@ import { ToastContainer } from 'react-toastify';
 import { AdminContext } from "./context/adminContext";
 import { Home } from "./pages/home";
 import { KitList } from "./pages/kitList";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import 'react-toastify/dist/ReactToastify.css';
+import { Login } from "./pages/login";
+import { auth } from "./firebaseConfig";
 
 function App () {
   const adminContext = useContext( AdminContext );
   if ( !adminContext ) throw new Error( 'useContext deve ser usado dentro de um AdminContextProvider' );
 
   const { brand } = adminContext;
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (!user) navigate("/login");
+    });
+
+    return () => unsubscribe();
+  }, [navigate]);
 
   return (
-    <BrowserRouter>
+    <>
       <ToastContainer />
 
-      <Header />
+      { location.pathname !== "/login" && <Header /> }
 
       <main>
-        <SideBar />
+        { location.pathname !== "/login" && <SideBar /> }
 
         <Routes>
           <Route path="/" element={ <Home /> } />
+
+          <Route path="/login" element={ <Login /> } />
 
           <Route path={ `/${ brand }/adicionar-item` } element={ <AddItem /> } />
 
@@ -37,7 +51,7 @@ function App () {
           <Route path={ `/${ brand }/kits-presentes` } element={ <KitList /> } />
         </Routes>
       </main>
-    </BrowserRouter>
+    </>
   )
 }
 

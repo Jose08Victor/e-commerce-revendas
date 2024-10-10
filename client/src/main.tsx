02b2ac007412236b7a471c10bc933f31d5e6deb6
José Victor <jose08.victor@gmail.com>
2024-10-10 20:23:30 -0300

    Feat:: AdminContextProvider and GlobalStyle added to main.tsx.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import { AdminContextProvider } from './context/adminContext.tsx';
+import { GlobalStyle } from './globalStyle.tsx';
 
-createRoot(document.getElementById('root')!).render(
+createRoot( document.getElementById( 'root' )! ).render(
   <StrictMode>
-    <App />
+    <AdminContextProvider>
+      <GlobalStyle/>
+      <App />
+    </AdminContextProvider>
   </StrictMode>,
-)
+);

d6cabc657b6f9c386c2ddb99b0f4e5e5ade12265
José Victor <jose08.victor@gmail.com>
2025-01-14 17:54:59 -0300

    Styles: Responsive styles have been added to the home page.

--- client/src/pages/home/styles.tsx
+++ client/src/pages/home/styles.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const Main = styled.main`
+    margin-bottom: 15px;
 `
 export const Ul = styled.ul`
     display: none;
@@ -34,15 +35,15 @@ export const Ul = styled.ul`
 export const CategoryField = styled.ul`
     display: none;
     align-items: center;
-    padding: 5px;
+    padding: 5px 0;
     gap: 10px;
     overflow-x: auto;
-    width: 95%;
-    margin: 0 auto;
+    width: 88%;
+    margin: 0 auto; 
 
     &::-webkit-scrollbar {
         height: 2px;
-        background-color: aliceblue;
+        background-color: #fff;
     }
 
     &::-webkit-scrollbar-thumb {
@@ -56,14 +57,14 @@ export const CategoryField = styled.ul`
         border: 2px solid;
         padding: 4px 12px;
         letter-spacing: 2px;
-        color: aliceblue;
+        color: #fff;
         background-color: ${ ( { theme } ) => theme.color };
         white-space: nowrap;
         font-weight: 600;
 
         &:hover {
             color: ${ ( { theme } ) => theme.color };
-            background-color: aliceblue;
+            background-color: #fff;
             border: 2px solid ${ ( { theme } ) => theme.color };
             cursor: pointer;
         }
@@ -77,23 +78,43 @@ export const CategoryField = styled.ul`
 export const Banner = styled.img`
     width: 96%;
     border-radius: 20px;
-    height: 280px;
     display: block;
     margin: 15px auto;
-    object-fit: cover;
+
+    @media(max-width: 500px) {
+        border-radius: 10px;
+    }
 `
 
 export const TitleOneContainer = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
-    margin: 0 30px;
+    padding: 10px 30px;
+    color: ${ ( { theme } ) => theme.color };
+    background-color: #fff;
+    border-start-start-radius: 30px;
+    border-start-end-radius: 30px;
+
+    @media (max-width: 500px) {
+        padding: 12px 20px;
+        border-start-start-radius: 20px;
+        border-start-end-radius: 20px;
+    }
 `
 
 export const TitleOne = styled.h1`
     font-weight: 600;
     font-size: 29px;
     letter-spacing: 1px;
+
+    @media (max-width: 768px) {
+        font-size: 25px;
+    }
+
+    @media (max-width: 500px) {
+        font-size: 18px;
+    }
 `
 
 export const TitleTwoContainer = styled.div`
@@ -101,14 +122,40 @@ export const TitleTwoContainer = styled.div`
     gap: 10px;
     align-items: center;
     padding: 5px 10px;
-    border: 1px solid black;
+    border: 1px solid ${ ( { theme } ) => theme.color };
+    background-color: ${ ( { theme } ) => theme.color };
     border-radius: 15px;
+    cursor: pointer;
+    transition: .3s ease-in-out;
+
+    &:hover {
+        background-color: #fff;
+        transform: scale(1.04);
+    }
+
+    @media (max-width: 768px) {
+        transform: scale(0.85);
+
+        &:hover {
+            transform: scale(0.9);
+        }
+    }
 `
 
 export const TitleTwo = styled.h1`
     font-size: 17px;
     font-weight: 600;
     letter-spacing: .5px;
+    color: #fff;
+    transition: .3s ease-in-out;
+
+    ${ TitleTwoContainer }:hover & {
+        color: ${ ( { theme } ) => theme.color };
+    }
+
+    @media (max-width: 500px) {
+        display: none;
+    }
 `
 
 export const Section = styled.section`
@@ -117,10 +164,15 @@ export const Section = styled.section`
         justify-content: center;
         flex-wrap: wrap;
         gap: 20px;
+        background-color: #fff;
+        border-end-start-radius: 30px;
+        border-end-end-radius: 30px;
+        padding: 10px;
         
         li {
-            background-color: aliceblue;
+            background-color: ${ ( { theme } ) => theme.backgroundColor };
             width: 240px;
+            min-height: 240px;
             border-radius: 15px;
             padding: 12px;
             display: flex;
@@ -145,33 +197,89 @@ export const Section = styled.section`
                 width: 100%;
                 border-radius: 10px;
                 margin-bottom: 15px;
-                border: 1px solid ${ ( { theme } ) => theme.color };
             }
 
-            div{
-                
-            p {
-                font-weight: 900;
-                font-size: 20px;
-                line-height: 10px;
-                color: ${ ( { theme } ) => theme.color };
-            }
+            div {
 
-            span {
-                font-size: 11px;
-                font-weight: 600;
+                p {
+                    font-weight: 900;
+                    font-size: 21px;
+                    line-height: 10px;
+                    color: ${ ( { theme } ) => theme.color };
+                }
+
+                span {
+                    font-size: 11px;
+                    font-weight: 600;
+                }
             }
         }
-             
-            svg {
-                position: absolute;
-                width: 40px;
-                background-color: ${ ( { theme } ) => theme.color };
-                border: 1px solid aliceblue;
-                border-radius: 50%;
-                top: 182px;
-                right: 18px;
+    }
+
+    @media (max-width: 800px) {
+        ul {
+            gap: 15px;
+            
+            li {
+                width: 200px;
+                min-height: 200px;
+                border-radius: 12px;
+                padding: 10px;
+
+                h3 {
+                    font-size: 13px;
+                    margin-bottom: 18px;
+                    line-height: 15px;
+                }
+
+                img {
+                    margin-bottom: 13px;
+                }
+
+                div {
+                    p {
+                        font-size: 19px;
+                    }
+
+                    span {
+                        font-size: 10px;
+                    }
+                }
             }
-        }
+        }   
+    }
+
+    @media (max-width: 500px) {
+        ul {
+            gap: 10px;
+            border-end-start-radius: 20px;
+            border-end-end-radius: 20px;
+            
+            li {
+                width: 160px;
+                border-radius: 9px;
+                padding: 8px;
+
+                h3 {
+                    font-size: 11px;
+                    line-height: 13px;
+                }
+
+                img {
+                    margin-bottom: 10px;
+                }
+
+                div {
+                    p {
+                        font-size: 16px;
+                        line-height: 4px;
+                    }
+
+                    span {
+                        font-size: 8px;
+                    }
+                }
+            }
+        }   
     }
 `

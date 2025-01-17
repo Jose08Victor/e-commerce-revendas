ecd25c786b28c1f843c4e4ee1b330e95422d19e0
José Victor <jose08.victor@gmail.com>
2025-01-17 14:17:51 -0300

    Feat: Enhance AdminContext for product and category management.

--- client/src/context/adminContext.tsx
+++ client/src/context/adminContext.tsx
@@ -1,6 +1,8 @@
 import { createContext, useState } from "react";
 import { ThemeProvider as StyledThemeProvider } from "styled-components";
-import { AdminContextProps, AdminContextProviderProps, ThemeType } from "../types";
+import { AdminContextProps, AdminContextProviderProps, ThemeType, List, Product } from "../types";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 export const AdminContext = createContext<AdminContextProps | null>( null );
 
@@ -20,11 +22,51 @@ export const AdminContextProvider = ( props: AdminContextProviderProps ) => {
 
     const url = "https://marlene-cosmeticos-server.onrender.com";
 
+    const [ brand, setBrand ] = useState<string>( "avon" );
+
     const [ isBag, setIsBag ] = useState( false );
 
     const [ isNav, setIsNav ] = useState( false );
 
-    const contextValue = { theme, setTheme, url, isBag, setIsBag, isNav, setIsNav };
+    const [ category, setCategory ] = useState( "Perfumes" );
+
+    const [ list, setList ] = useState<List>( {
+        avonProducts: [],
+        avonKits: [],
+        naturaProducts: [],
+        naturaKits: [],
+        magazine: []
+    } );
+
+    const [ inTheBag, setInTheBag ] = useState<Product[]>( [] );
+
+    const getAvonProductList = async () => {
+        try {
+            const response = await axios.get( `${ url }/api/avon/productList` );
+
+            if ( !response.data.success ) toast.error( response.data.message );
+            else setList( list => ( { ...list, avonProducts: response.data.data } ) );
+        } catch ( error ) {
+            console.log( error );
+            toast.error( "Falha ao conectar com o servidor" );
+        }
+    };
+
+    const getNaturaProductList = async () => {
+        try {
+            const response = await axios.get( `${ url }/api/natura/productList` );
+
+            if ( !response.data.success ) toast.error( response.data.message );
+            else setList( list => ( { ...list, naturaProducts: response.data.data } ) );
+        } catch ( error ) {
+            console.log( error );
+            toast.error( "Falha ao conectar com o servidor" );
+        }
+    };
+
+    
+
+    const contextValue = { theme, setTheme, url, isBag, setIsBag, isNav, setIsNav, category, setCategory, list, brand, setBrand, inTheBag, setInTheBag, getAvonProductList, getNaturaProductList };
 
     return (
         <AdminContext.Provider value={ contextValue }>

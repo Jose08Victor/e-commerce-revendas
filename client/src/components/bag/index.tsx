db6a226a3ed9b622c6e3c8edcc5a7b42e14a442c
José Victor <jose08.victor@gmail.com>
2025-01-17 14:27:44 -0300

    Feat: Refactor Bag component to dynamically render products and optimize code.

--- client/src/components/bag/index.tsx
+++ client/src/components/bag/index.tsx
@@ -1,13 +1,13 @@
 import { Background, BagContainer, PriceContainer, TitleContainer, Prices } from "./styles";
-import avonIcon from "../../assets/avonIcon.png";
 import { useContext } from "react";
 import { AdminContext } from "../../context/adminContext";
+import { TrashCanSVG } from "../svgContainer";
 
 export const Bag = () => {
     const adminContext = useContext( AdminContext );
     if ( !adminContext ) throw new Error( 'useContext deve ser usado dentro de um AdminContextProvider' );
 
-    const { setIsBag } = adminContext;
+    const { setIsBag, inTheBag } = adminContext;
 
     return (
         <Background>
@@ -15,129 +15,37 @@ export const Bag = () => {
                 <TitleContainer>
                     <h1>Minha Sacola</h1>
 
-                    <p onClick={() => setIsBag(false)} >x</p>
+                    <p onClick={ () => setIsBag( false ) } >x</p>
                 </TitleContainer>
 
                 <ul>
-                    <li>
-                        <img src={ avonIcon } alt="" />
+                    { inTheBag.map( ( product, index ) => {
+                        return (
+                            <li key={ index }>
+                                <img src={ product.imageURL } alt={ product.name } />
 
-                        <div className="product-features">
-                            <div className="product-title-container">
-                                <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi doloremque enim earum jas.</h3>
+                                <div className="product-features">
+                                    <div className="product-title-container">
+                                        <h3>{ product.name }</h3>
 
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0L284.2 0c12.1 0 23.2 6.8 28.6 17.7L320 32l96 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 96C14.3 96 0 81.7 0 64S14.3 32 32 32l96 0 7.2-14.3zM32 128l384 0 0 320c0 35.3-28.7 64-64 64L96 512c-35.3 0-64-28.7-64-64l0-320zm96 64c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16z" /></svg>
-                            </div>
+                                        <TrashCanSVG />
+                                    </div>
 
-                            <div className="price-container">
-                                <div>
-                                    <button>+</button>
+                                    <div className="price-container">
+                                        <div>
+                                            <button>+</button>
 
-                                    <span>1</span>
+                                            <span>1</span>
 
-                                    <button>−</button>
+                                            <button>−</button>
+                                        </div>
+
+                                        <p>{ Intl.NumberFormat( 'pt-BR', { style: 'currency', currency: 'BRL' } ).format( product.price ) }</p>
+                                    </div>
                                 </div>
-
-                                <p>R$ 30,00</p>
-                            </div>
-                        </div>
-                    </li>
-
-                    <li>
-                        <img src={ avonIcon } alt="" />
-
-                        <div className="product-features">
-                            <div className="product-title-container">
-                                <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi doloremque enim earum jas.</h3>
-
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0L284.2 0c12.1 0 23.2 6.8 28.6 17.7L320 32l96 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 96C14.3 96 0 81.7 0 64S14.3 32 32 32l96 0 7.2-14.3zM32 128l384 0 0 320c0 35.3-28.7 64-64 64L96 512c-35.3 0-64-28.7-64-64l0-320zm96 64c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16z" /></svg>
-                            </div>
-
-                            <div className="price-container">
-                                <div>
-                                    <button>+</button>
-
-                                    <span>1</span>
-
-                                    <button>−</button>
-                                </div>
-
-                                <p>R$ 30,00</p>
-                            </div>
-                        </div>
-                    </li>
-
-                    <li>
-                        <img src={ avonIcon } alt="" />
-
-                        <div className="product-features">
-                            <div className="product-title-container">
-                                <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi doloremque enim earum jas.</h3>
-
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0L284.2 0c12.1 0 23.2 6.8 28.6 17.7L320 32l96 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 96C14.3 96 0 81.7 0 64S14.3 32 32 32l96 0 7.2-14.3zM32 128l384 0 0 320c0 35.3-28.7 64-64 64L96 512c-35.3 0-64-28.7-64-64l0-320zm96 64c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16z" /></svg>
-                            </div>
-
-                            <div className="price-container">
-                                <div>
-                                    <button>+</button>
-
-                                    <span>1</span>
-
-                                    <button>−</button>
-                                </div>
-
-                                <p>R$ 30,00</p>
-                            </div>
-                        </div>
-                    </li>
-
-                    <li>
-                        <img src={ avonIcon } alt="" />
-
-                        <div className="product-features">
-                            <div className="product-title-container">
-                                <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi doloremque enim earum jas.</h3>
-
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0L284.2 0c12.1 0 23.2 6.8 28.6 17.7L320 32l96 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 96C14.3 96 0 81.7 0 64S14.3 32 32 32l96 0 7.2-14.3zM32 128l384 0 0 320c0 35.3-28.7 64-64 64L96 512c-35.3 0-64-28.7-64-64l0-320zm96 64c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16z" /></svg>
-                            </div>
-
-                            <div className="price-container">
-                                <div>
-                                    <button>+</button>
-
-                                    <span>1</span>
-
-                                    <button>−</button>
-                                </div>
-
-                                <p>R$ 30,00</p>
-                            </div>
-                        </div>
-                    </li>
-
-                    <li>
-                        <img src={ avonIcon } alt="" />
-
-                        <div className="product-features">
-                            <div className="product-title-container">
-                                <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi doloremque enim earum jas.</h3>
-
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0L284.2 0c12.1 0 23.2 6.8 28.6 17.7L320 32l96 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 96C14.3 96 0 81.7 0 64S14.3 32 32 32l96 0 7.2-14.3zM32 128l384 0 0 320c0 35.3-28.7 64-64 64L96 512c-35.3 0-64-28.7-64-64l0-320zm96 64c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16z" /></svg>
-                            </div>
-
-                            <div className="price-container">
-                                <div>
-                                    <button>+</button>
-
-                                    <span>1</span>
-
-                                    <button>−</button>
-                                </div>
-
-                                <p>R$ 30,00</p>
-                            </div>
-                        </div>
-                    </li>
+                            </li>
+                        )
+                    } ) }
                 </ul>
 
                 <PriceContainer>
